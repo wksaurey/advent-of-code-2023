8561f5f85d132a61f3ef26f9713dd42db6de08da
wksaurey <70457200+wksaurey@users.noreply.github.com>
2023-12-26 22:00:14 -0700

    finished day1 part 2

--- day1.py
+++ day1.py
@@ -4,17 +4,67 @@ numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine
 calibrations = read_stripped_lines('input/day1.text')
 total = 0
 for calibration in calibrations:
-    print(calibration)
-    for char in calibration:
+    print(f'Calibration: {calibration}')
+    for index, char in enumerate(calibration):
         if char.isdigit():
             first = char
+            print(f'First: {first}')
             break
-    for char in reversed(calibration):
+        # check for spelled out numbers
+        startIndex = index
+        endIndex = index + 1
+        possibleNumbers = numbers
+        newNumbers = []
+        first = None
+        while(len(possibleNumbers) >= 1):
+            for number in possibleNumbers:
+                if(calibration[startIndex:endIndex] == number[0:endIndex - startIndex]):
+                    if len(number) == endIndex - startIndex:
+                        first = str(numbers.index(number)+1)
+                        print(f'First: {first}')
+                        break
+                    newNumbers.append(number)
+            if(first): 
+                break
+            possibleNumbers = newNumbers
+            newNumbers = []
+            endIndex += 1
+        if(first):
+            break
+        
+    print(f'Reversed Calibration: {calibration[::-1]}')
+    reversedCalibration = calibration[::-1]
+    for index, char in enumerate(reversedCalibration):
         if char.isdigit():
             second = char
+            print(f'Second: {second}')
             break
+        # check for reversed spelled out numbers
+        startIndex = index
+        endIndex = index + 1
+        possibleNumbers = numbers
+        newNumbers = []
+        second = None
+        while(len(possibleNumbers) >= 1):
+            for number in possibleNumbers:
+                number = number[::-1]
+                if(reversedCalibration[startIndex:endIndex] == number[0:endIndex - startIndex]):
+                    if len(number) == endIndex - startIndex:
+                        second = str(numbers.index(number[::-1])+1)
+                        print(f'Second: {second}')
+                        break
+                    newNumbers.append(number[::-1])
+            if(second): 
+                break
+            possibleNumbers = newNumbers
+            newNumbers = []
+            endIndex += 1
+        if(second):
+            break
+
+    # combine first and second
     value = int(first+second)
-    print(value)
+    print(f'End Value: {value} \n')
     total += value
 
 print(f'Total: {total}')
