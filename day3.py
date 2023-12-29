ee901c83bcb8384e42c7978201e733ea8bb88cd8
wksaurey <70457200+wksaurey@users.noreply.github.com>
2023-12-29 16:38:39 -0700

    started updating day 3 part 2

--- day3.py
+++ day3.py
@@ -2,67 +2,97 @@ from util import read_stripped_lines
 
 parts = read_stripped_lines('input/day3.text')
 print(parts)
+gearOne = None
+gearTwo = None
 
-partNumberSum = 0
-for lineIndex, line in enumerate(parts):
-    numLength = 0
-    for index, char in enumerate(line):
-        if numLength != 0:
-            numLength -= 1
-            continue
-        if not char.isdigit():
-            continue
-        # get num length
-        numLength = 1
-        while(index+numLength < len(line) and line[index+numLength].isdigit()):
-            numLength += 1
-        partNumber = int(line[index:index+numLength])
-
-        # check left
-        if index != 0:
-            if line[index-1]!= '.' and not line[index-1].isdigit():
-                print(f'{partNumber} is touching {line[index-1]}')
-                partNumberSum += partNumber
+def main():
+    partNumberSum = 0
+    for lineIndex, line in enumerate(parts):
+        numLength = 0
+        for index, char in enumerate(line):
+            if numLength != 0:
+                numLength -= 1
+                continue
+            if char != '*':
                 continue
 
-        # check right
-        if index+numLength < len(line):
-            if line[index+numLength] != '.' and not line[index+numLength].isdigit():
-                print(f'{partNumber} is touching {line[index+numLength]}')
-                partNumberSum += partNumber
-                continue
-
-        # check up along length
-        if lineIndex != 0:
-            startbuffer = 0
-            endbuffer = 0
+            # check left
             if index != 0:
-                startbuffer = 1
-            if index+numLength < len(line):
-                endbuffer = 1
-            isPartNumber = False
-            for tempIndex in range(index-startbuffer, index+numLength+endbuffer):
-                if parts[lineIndex-1][tempIndex] != '.' and not parts[lineIndex-1][tempIndex].isdigit():
-                    isPartNumber = True
-                    print(f'{partNumber} is touching {parts[lineIndex-1][tempIndex]}')
-                    partNumberSum += partNumber
-                    break
-            if(isPartNumber):
+                num = getNum(lineIndex, index-1)
+                if num != None:
+                    setGear(num)
+
+            # check right
+            if index+1 < len(line):
+                num = getNum(lineIndex, index+1)
+                if num != None:
+                    setGear(num)
+
+            # TODO: update up and down
+            # check top 3
+            if lineIndex != 0:
+                startbuffer = 0
+                endbuffer = 0
+                if index != 0:
+                    startbuffer = 1
+                if index+1 < len(line):
+                    endbuffer = 1
                 isPartNumber = False
-                continue
-            
-        # check down along length
-        if lineIndex != len(line)-1:
-            startbuffer = 0
-            endbuffer = 0
-            if index != 0:
-                startbuffer = 1
-            if index+numLength < len(line):
-                endbuffer = 1
-            for tempIndex in range(index-startbuffer, index+numLength+endbuffer):
-                if parts[lineIndex+1][tempIndex] != '.' and not parts[lineIndex+1][tempIndex].isdigit():
-                    print(f'{partNumber} is touching {parts[lineIndex+1][tempIndex]}')
-                    partNumberSum += partNumber
-                    break
+                for tempIndex in range(index-startbuffer, index+numLength+endbuffer):
+                    if parts[lineIndex-1][tempIndex] != '.' and not parts[lineIndex-1][tempIndex].isdigit():
+                        isPartNumber = True
+                        print(f'{partNumber} is touching {parts[lineIndex-1][tempIndex]}')
+                        partNumberSum += partNumber
+                        break
+                if(isPartNumber):
+                    isPartNumber = False
+                    continue
+                
+            # check down along length
+            if lineIndex != len(line)-1:
+                startbuffer = 0
+                endbuffer = 0
+                if index != 0:
+                    startbuffer = 1
+                if index+numLength < len(line):
+                    endbuffer = 1
+                for tempIndex in range(index-startbuffer, index+numLength+endbuffer):
+                    if parts[lineIndex+1][tempIndex] != '.' and not parts[lineIndex+1][tempIndex].isdigit():
+                        print(f'{partNumber} is touching {parts[lineIndex+1][tempIndex]}')
+                        partNumberSum += partNumber
+                        break
 
-print(f'Sum: {partNumberSum}')
+    print(f'Sum: {partNumberSum}')
+
+
+def getNum(lineIndex, index):
+    if lineIndex < 0 or lineIndex >= len(parts):
+        return None
+    if index < 0 or index >= len(parts[lineIndex]):
+        return None
+    if not parts[lineIndex][index].isdigit():
+        return None
+
+    startIndex = index
+    while(startIndex > 0):
+        if parts[lineIndex][startIndex-1].isdigit():
+            startIndex -= 1
+
+    endIndex = index
+    while(endIndex < len(parts[lineIndex])):
+        if parts[lineIndex][endIndex+1].isdigit():
+            endIndex += 1
+
+    return int(parts[lineIndex][startIndex:endIndex+1])
+
+def setGear(num):
+    if gearOne == None:
+        gearOne = num
+        print(f'Gear One: {num}')
+    elif gearTwo == None:
+        gearTwo = num
+        print(f'GearTwo: {num}')
+    else:
+        print('More than two gears were found')
+
+main()
