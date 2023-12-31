0fdf59e8dced0b5aa7dfcb22c44106e09fe71c1e
wksaurey <70457200+wksaurey@users.noreply.github.com>
2023-12-31 07:09:56 -0700

    finished day4 part 2

--- day4.py
+++ day4.py
@@ -1,8 +1,10 @@
 with open('input/day4.text') as file:
     games = file.read().splitlines()
 
-totalScore = 0
+gameCounts = [1] * len(games)
+
 for gameIndex, game in enumerate(games):
+    print(gameCounts)
     game = game.split(':')[1].strip().split('|')
     winningNums = list(map(int, game[0].split()))
     nums = list(map(int, game[1].split()))
@@ -10,11 +12,19 @@ for gameIndex, game in enumerate(games):
     gamescore = 0
     for num in nums:
         if num in winningNums:
-            if gamescore == 0:
-                gamescore = 1
-            else:
-                gamescore = gamescore * 2
-    totalScore += gamescore
+            gamescore += 1
     print(f'Game {gameIndex}: {gamescore}')
+    for tempGameIndex in range(gameIndex+1, gameIndex + gamescore + 1):
+        if tempGameIndex >= len(games):
+            break
+        if tempGameIndex == 5:
+            print('Something is wrong...')
+        gameCounts[tempGameIndex] += gameCounts[gameIndex]
+        
+gameCountSum = 0
+for index, gameCount in enumerate(gameCounts):
+    print(f'Game {index+1} Count: {gameCount}')
+    gameCountSum += gameCount
 
-print(f'\nTotal Score: {totalScore}')
+
+print(f'\nTotal Count: {gameCountSum}')
