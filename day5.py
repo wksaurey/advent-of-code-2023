eaa808a8c3b2a387b204a95778da566478dcf3df
Kolter Saurey <Kolter.Saurey@asirobots.com>
2024-01-02 16:01:06 -0700

    Started working on processing seed ranges for part 2

--- day5.py
+++ day5.py
@@ -1,30 +1,45 @@
-seeds = []
-conversions = []
-with open('input/day5.text') as file:
-    for line in file:
-        if line == '\n':
-            continue
-        if ':' in line:
-            if 'map' in line:
-                conversions.append([])
+def main():
+    seeds = getSeeds()
+    conversions = getConversions()
+
+    START = 0
+    RANGE = 1
+    DELTA = 2
+
+    for conversionIndex, conversionValues in enumerate(conversions):
+        print(f'Current Conversion: {conversionValues}')
+        print(f'Initial Seeds: {seeds}')
+        for seedIndex, seedRange in enumerate(seeds):
+            for conversionRange in conversionValues:
+                if seedRange[START] >= conversionRange[START] and seedRange[START] + seedRange[RANGE]-1 <= conversionRange[START] + conversionRange[RANGE]-1:
+                    seeds[seedIndex][START] = seedRange[START]+conversionRange[DELTA]
+
+
+    print(f'Closest Location: {min(seeds)}')
+
+def getSeeds(): 
+    seeds = []
+    with open('input/day5test.text') as file:
+        line = file.readline()
+        seedValues = list(map(int, line.split(':')[1].strip().split()))
+        for seedIndex, seedValue in enumerate(seedValues):
+            if seedIndex % 2 != 0 :
+                continue
+            seeds.append([seedValue, seedValues[seedIndex+1]])
+    return seeds
+
+def getConversions():
+    conversions = []
+    with open('input/day5test.text') as file:
+        for line in file:
+            if line == '\n':
+                continue
+            if ':' in line:
+                if 'map' in line:
+                    conversions.append([])
             else:
-                seeds = list(map(int, line.split(':')[1].strip().split()))
-        else:
-            values = list(map(int, line.strip().split()))
-            conversions[-1].append([values[0]-values[1], values[1], values[1]+values[2]-1])
-# print(conversions)
+                values = list(map(int, line.strip().split()))
+                conversions[-1].append([values[1], values[2], values[0]-values[1]])
+    return conversions
 
-DELTA = 0
-START = 1
-END = 2
-
-for conversionIndex, conversionValues in enumerate(conversions):
-    print(f'Current Conversion: {conversionValues}')
-    for seedIndex, seed in enumerate(seeds):
-        for conversionValue in conversionValues:
-            if seed >= conversionValue[START] and seed <= conversionValue[END]:
-                seeds[seedIndex] = seed + conversionValue[DELTA]
-                print(f'{seed} converted to {seeds[seedIndex]}')
-    print(f'Seeds: {seeds}')
-
-print(f'Closest Location: {min(seeds)}')
+main()
