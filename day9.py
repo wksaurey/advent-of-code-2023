052a6a2d28d1b3482b6c37be8b22611987b475cb
Kolter Saurey <Kolter.Saurey@asirobots.com>
2024-01-04 14:36:35 -0700

    finished day9 part 1

---
+++ day9.py
@@ -0,0 +1,47 @@
+import time
+from util import read_stripped_lines
+
+def main():
+    dataSets = [list(map(int, set.split())) for set in read_stripped_lines('input/day9.text')]
+    print(dataSets)
+
+    extrapolatedSum = 0
+    for dataSet in dataSets:
+        derivatives = getDerivatives(dataSet)
+        extrapolatedValue = getExtrapolatedValue(derivatives)
+        print(f'Extrapolated Value: {extrapolatedValue}')
+        extrapolatedSum += extrapolatedValue
+    print(f'Extrapolated Sum: {extrapolatedSum}')
+
+def getExtrapolatedValue(derivatives):
+    for index, derivative in enumerate(reversed(derivatives)):
+        if index == 0:
+            lastValue = derivative[-1]
+            continue
+        derivative.append(derivative[-1] + lastValue)
+        lastValue = derivative[-1]
+    return derivatives[0][-1]
+
+def getDerivatives(dataSet):
+    derivatives = [dataSet]
+    while(True):
+        derivative = []
+        for index, value in enumerate(derivatives[-1]):
+            if index == 0:
+                continue
+            derivative.append(value - derivatives[-1][index-1])
+        if(isLastDerivative(derivative)):
+            break
+        derivatives.append(derivative)
+    return derivatives
+
+
+def isLastDerivative(values):
+    lastDerivative = True
+    for value in values:
+        if value != 0:
+            lastDerivative = False
+            break
+    return lastDerivative
+
+main()
